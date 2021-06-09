bbe4415b188131cae5703143092c3110fe8977e1
Josué Casiá <josuecasia201831432@cunoc.edu.gt>
2021-06-08 21:11:08 -0600

    agregado_ejercicio1.java

---
+++ ejercicio1.java
@@ -0,0 +1,10 @@
+public class ejercicio1 {
+	public static void main(String args[] ){
+		int numero1=5;
+		int numero2=10;
+		System.println("los numeros son numero1 :"+numero1);
+		System.println("los numeros son numero1 :"+numero1);
+		int suma = numero1 + numero2;
+		System.println("la suma de los numeros es: "suma);
+	}
+}
