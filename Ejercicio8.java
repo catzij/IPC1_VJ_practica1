53b9d1dee5b072e52e1816513857d2785b99b826
Josué Casiá <josuecasia201831432@cunoc.edu.gt>
2021-06-09 00:34:35 -0600

    Agreagado Ejercicio8.java

---
+++ Ejercicio8.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Ejercicio8 {
+	
+	public static void main(String args[] ){
+		Scanner read = new Scanner(System.in);
+		int numero1 = 10;
+		int numero2 = 20;
+		int numero3 = 30;
+		int promedio;
+
+		promedio = (numero1 + numero2 + numero3)/3;
+
+		System.out.println( "numeros ingresados "+numero1 +" "+ numero2+" "+ numero3+
+							"\nel promedio de los numeros es "+promedio);
+
+	}
+}
+
+	
+
