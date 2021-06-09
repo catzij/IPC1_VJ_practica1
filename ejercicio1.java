72bea095fbf3830043c8d1be586f6fee54aa05a1
Josué Casiá <josuecasia201831432@cunoc.edu.gt>
2021-06-08 21:25:15 -0600

    corregido ejercicio1.java

--- ejercicio1.java
+++ ejercicio1.java
@@ -1,10 +1,16 @@
 public class ejercicio1 {
 	public static void main(String args[] ){
-		int numero1=5;
-		int numero2=10;
-		System.println("los numeros son numero1 :"+numero1);
-		System.println("los numeros son numero1 :"+numero1);
-		int suma = numero1 + numero2;
-		System.println("la suma de los numeros es: "suma);
+
+		int numero1 = 5;
+		int numero2 = 10;
+		int suma;
+
+		suma = numero1 + numero2 ;
+
+		System.out.println("numero1 : "+numero1);
+		System.out.println("numero1 : "+numero1);
+		System.out.println("la suma de los numeros es: "+suma);
+
+
 	}
 }
